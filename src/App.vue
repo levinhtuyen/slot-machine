7282a7e8faabbd2c24131ca430b3575143cdf0d8
tuyen-go2joy <tuyen.le@go2joy.vn>
2022-08-27 15:26:16 +0700

    create  prject

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <div id="page">
+      <div class="main">
+        <!-- Slots -->
+        <div class="numbers">
+          <div class="slots" id="slots_tens">
+            <div class="number-wrapper"></div>
+          </div>
+          <div class="slots" id="slots_units">
+            <div class="number-wrapper"></div>
+          </div>
+          <div class="slots" id="slots_queue">
+            <div class="number-wrapper"></div>
+          </div>
+        </div>
+      </div>
+      <div class="button">
+        <!--<input type="button" value="spin!" onClick="go(Math.floor(Math.random() * 10),Math.floor(Math.random() * 10));">-->
+        <div id="equation" class="done">
+          <div id="shoulder"><div id="arm"></div></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/assets/js/slot.js
@@ -0,0 +1,79 @@
+/*
+*
+*	jQuery Slot Machine
+*
+*/
+var height_slot_number = '100';
+
+function go(tens,units){
+	addSlots($("#slots_units .number-wrapper"));
+	addSlots($("#slots_units .number-wrapper"));
+	moveSlots($("#slots_units .number-wrapper"),tens);
+	addSlots($("#slots_tens .number-wrapper"));
+	addSlots($("#slots_tens .number-wrapper"));
+  moveSlots($("#slots_tens .number-wrapper"), tens);
+  addSlots($("#slots_queue .number-wrapper"));
+	addSlots($("#slots_queue .number-wrapper"));
+	moveSlots($("#slots_queue .number-wrapper"),tens);
+}
+
+$(document).ready(function(){
+	addSlots($("#slots_units .number-wrapper"));
+  addSlots($("#slots_tens .number-wrapper"));
+  addSlots($("#slots_queue .number-wrapper"));
+    $('#arm').click(function(e) {
+			var arm = $(this).addClass('clicked');
+      delay = setTimeout(function() { arm.removeClass('clicked');}, 500);
+      e.preventDefault();
+      go(Math.floor(Math.random() * 10));
+	});
+});
+
+
+function addSlots (jqo) {
+  let data = [
+    {
+      src: 'https://staging-s3.go2joy.vn/350w/luckywheel/171_1660547786_62f9f2cac4f9c.png',
+      sn: 1,
+      text: '11k'
+    },{
+      src: 'https://staging-s3.go2joy.vn/350w/luckywheel/171_1660547834_62f9f2fa3da7f.png',
+      sn: 2,
+      text: '33k'
+    },{
+      src: 'https://staging-s3.go2joy.vn/350w/luckywheel/171_1660547875_62f9f3232bf9e.png',
+      sn: 3,
+      text: '44k'
+    },{
+      src: 'https://staging-s3.go2joy.vn/350w/luckywheel/171_1660547909_62f9f345c66c7.png',
+      sn: 4,
+      text: '55k'
+    },{
+      src: 'https://staging-s3.go2joy.vn/350w/luckywheel/171_1660547786_62f9f2cac4f9c.png',
+      sn: 5,
+      text: '111k'
+    },{
+      src: 'https://staging-s3.go2joy.vn/350w/luckywheel/171_1660548055_62f9f3d786855.png',
+      sn: 6,
+      text: 'Có cái nịt'
+    }
+  ]
+	for(var i = 0; i < data.length; i++){
+		jqo.append(`<div class='slot'><img src="${data[i].src}" class="el-image__inner" style="object-fit: scale-down;"></div>`);
+	}
+}
+
+function moveSlots(jqo,num){
+	var time	= 6500;
+	var number	= num;
+	time		+= Math.round(Math.random()*1000);
+	jqo.stop(true,true);
+
+	var num_slot    = Math.round((jqo.find('.slot').length)/20);
+	var margin_top  = ((num_slot -1) * (height_slot_number * 10)) + (num * height_slot_number);
+
+	jqo.animate(
+		{"margin-top":"-"+ margin_top +"px"},
+		{'duration' : time, 'easing' : "easeOutElastic"}
+	);
+}
